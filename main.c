e23dacb64a310716ae6b1961dbcbbfcac7f769c5
Brian Martin <bmartin@twitter.com>
2020-01-14 14:21:28 -0800

    next

--- main.c
+++ main.c
@@ -14,10 +14,10 @@ int main (int argc, char *argv[])
 		return 1;
 	}
 	
-	IT8951DisplayExample();
+	// IT8951DisplayExample();
 	IT8951DisplayExample2();
-	printf("IT8951_GUI_Example\n");
-	IT8951_GUI_Example();
+	// printf("IT8951_GUI_Example\n");
+	// IT8951_GUI_Example();
 	
 	
 	if (argc != 4)
